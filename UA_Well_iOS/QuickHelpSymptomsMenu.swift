ec6d53f4f2000dca366be970806dc517a5c4b644
Наталья Гусарова <strixns@gmail.com>
2024-11-30 14:30:20 +0100

    Added one more storyboard and QuickHelp jsons

---
+++ UA_Well_iOS/QuickHelpSymptomsMenu.swift
@@ -0,0 +1,8 @@
+//
+//  QuickHelpSymptomsMenu.swift
+//  UA_Well_iOS
+//
+//  Created by Наталья Гусарова on 30.11.2024.
+//
+
+import Foundation
---
+++ UA_Well_iOS/Quick_help_exercise.swift
@@ -0,0 +1,8 @@
+//
+//  Quick_help_exercise.swift
+//  UA_Well_iOS
+//
+//  Created by Наталья Гусарова on 30.11.2024.
+//
+
+import Foundation
