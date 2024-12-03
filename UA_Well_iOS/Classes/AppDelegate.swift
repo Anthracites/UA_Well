47069cf8f460c99a8c3796bfaae86bc30eabddf7
Наталья Гусарова <strixns@gmail.com>
2024-12-03 23:46:57 +0100

    Change orientation ban

--- UA_Well_iOS/Classes/AppDelegate.swift
+++ UA_Well_iOS/Classes/AppDelegate.swift
@@ -9,13 +9,8 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
 
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
 
     // MARK: UISceneSession Lifecycle
 
@@ -30,6 +25,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+           window = UIWindow(frame: UIScreen.main.bounds)
+           let splashVC = LaunchScreen()
+           window?.rootViewController = splashVC
+           window?.makeKeyAndVisible()
+           return true
+       }
+
 
 
 }
---
+++ UA_Well_iOS/Classes/LaunchScreen.swift
@@ -0,0 +1,32 @@
+import Foundation
+import UIKit
+
+class LaunchScreen: UIViewController {
+
+    let logoImageView = UIImageView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        logoImageView.image = UIImage(named: "logo")
+        logoImageView.contentMode = .scaleAspectFit
+        view.addSubview(logoImageView)
+        
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+        
+        animatePulsing()
+    }
+
+    func animatePulsing() {
+        UIView.animate(withDuration: 1.0, delay: 0, options: [.repeat, .autoreverse], animations: {
+            self.logoImageView.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+        })
+    }
+}
