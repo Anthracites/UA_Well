e6fff264742599405a5b1ef57759a055bf27a4fe
Наталья Гусарова <strixns@gmail.com>
2025-01-10 15:40:36 +0100

    Fixed scroll view on AboutUsAndContactUs

--- UA_Well_iOS/Classes/AboutUsAndContactUs.swift
+++ UA_Well_iOS/Classes/AboutUsAndContactUs.swift
@@ -3,14 +3,32 @@ import Foundation
 
 class AboutUsAndContactUs: UIViewController {
     @IBOutlet weak var okButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet var contentViews: [UIView]!
+    @IBOutlet var generalView: UIView!
+    
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         okButton.addTarget(self, action: #selector(BackToPreviousScreen), for: .touchUpInside)
+        // Установка contentSize для UIScrollView
+        ScrollSetup()
     }
     
+    @objc func ScrollSetup()
+    {
     
+        scrollView.contentSize = CGSize(width: generalView.frame.width, height: generalView.frame.height)
+
+        // Отключение горизонтальной прокрутки
+        scrollView.alwaysBounceHorizontal = false
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.isScrollEnabled = true
+        // Установка contentInsetAdjustmentBehavior
+        scrollView.contentInsetAdjustmentBehavior = .never
+    }
+
     @objc func BackToPreviousScreen()
     {
         dismiss(animated: true, completion: nil)
