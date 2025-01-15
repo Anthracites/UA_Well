6cd4df843d3530661c6a45a7b2eee98d6b2cd112
Наталья Гусарова <strixns@gmail.com>
2025-01-15 19:07:07 +0100

    Added json parser

---
+++ UA_Well_iOS/Classes/TranslationDownloader.swift
@@ -0,0 +1,10 @@
+import Foundation
+import UIKit
+
+public class TranslationDownloader {
+    
+    var Translations: [Translation] = []
+    var currentLanguage:String;
+    
+    
+}
--- UA_Well_iOS/Structures/Translation.swift
+++ UA_Well_iOS/Structures/Translation.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct LocalData
+struct Translation
 {
     var currentLanguage:String;
     
