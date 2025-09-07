4db52ebc9a278986e8a4ff3bd8efc279384b7254
Наталья Гусарова <strixns@gmail.com>
2025-09-07 17:29:26 +0200

    LTW day description screen's layout fix. (LTWDayDescription)

--- UA_Well_iOS/Classes/Interface/LTWDayDescription.swift
+++ UA_Well_iOS/Classes/Interface/LTWDayDescription.swift
@@ -6,11 +6,14 @@ class LTWDayDescription:  UIViewController, UICollectionViewDataSource, UICollec
     var _previousScreenName = "LongTimeWork"
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var titleText: UILabel!
-    @IBOutlet weak var descriptionText: UITextView!
-    @IBOutlet weak var scrollView: UIView!
+    @IBOutlet weak var descriptionText: AutoResizingTextView!
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var _collectionView: UICollectionView!
     @IBOutlet weak var alarmTitle: UILabel!
     @IBOutlet weak var LTWAlarm:  UISwitch!
+    @IBOutlet weak var header: UIView!
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var dayTitle: UIView!
     var TherapyDay: Int?
     var LTWDuration: Int?
     var LTWAlarmTime: String!
@@ -18,9 +21,14 @@ class LTWDayDescription:  UIViewController, UICollectionViewDataSource, UICollec
     var currentTranslation: Translation!
     var currentLTWDay: Int!
     
+    private var exerciseTextHeightConstraint: NSLayoutConstraint?
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        TherapyDay = LTWManager.shared.CurrentDay
+        ConfigCollectionView()
         _collectionView.register(UINib(nibName: "CustomCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "CustomCollectionViewCell")
         _collectionView.dataSource = self
         _collectionView.delegate = self
@@ -31,8 +39,19 @@ class LTWDayDescription:  UIViewController, UICollectionViewDataSource, UICollec
         LTWAlarm.addTarget(self, action: #selector(SaveOptions), for: .valueChanged)
         LTWAlarm.isOn = UserDefaults.standard.bool(forKey: "LTWAlarm")
         SaveOptions()
-        TherapyDay = LTWManager.shared.CurrentDay
         TherapyProgressTracker.shared.markTodayAsCompleted(for: .LTW)
+        
+        let layoutConfig = LayoutConfigurator.Config(
+            parentView: view,
+            header: header,
+            scrollView: scrollView,
+            contentView: contentView,
+            title:dayTitle,
+            exerciseText: descriptionText,
+            collectionView: _collectionView,
+            collectionViewItemsCount: buttonLabels.count + 1
+        )
+        exerciseTextHeightConstraint = LayoutConfigurator.configure(using: layoutConfig)
     }
     
     func TranslateButton(_currentButton: UIButton)
@@ -81,7 +100,20 @@ class LTWDayDescription:  UIViewController, UICollectionViewDataSource, UICollec
         self.present(secondVC, animated: true, completion: nil)
     }
     
+        func ConfigCollectionView()
+    {
+        
+        if (TherapyDay != 0)
+        {
+            buttonLabels = ["AboutUsAndContactUs"]
+        }
+        else {
+            
+        }
+    
+    }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
         return buttonLabels.count
     }
     
@@ -89,16 +121,38 @@ class LTWDayDescription:  UIViewController, UICollectionViewDataSource, UICollec
         // _collectionView.backgroundColor = .cyan
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CustomCollectionViewCell", for: indexPath) as! CustomCollectionViewCell
         let newButoon: UIButton = cell.MenuButton
-        cell.copyButtonProperties(targetButton: newButoon, isFilledButton: true)
         newButoon.tag = indexPath.item
         newButoon.setTitle(buttonLabels[indexPath.item], for: .normal)
         TranslateButton(_currentButton: newButoon)
+        cell.copyButtonProperties(targetButton: newButoon, isFilledButton: true)
+        cell.adjustFontSize(for: newButoon)
         newButoon.addTarget(self, action: #selector(OnClickMenuButton), for: .touchUpInside)
-        cell.contentMode = .center
         
-        if ((TherapyDay != 0)&&(indexPath.item == 0))
-        {
-            newButoon.isHidden = true
+        cell.contentMode = .center
+
+        
+        if let backgroundImage = newButoon.backgroundImage(for: .normal) {
+            
+            // Задаём фиксированную высоту кнопки
+            let targetHeight: CGFloat = UIScreen.main.bounds.width < 400 ? 80 : 120
+            let buttonHeight = targetHeight
+
+            // Вычисляем ширину кнопки по высоте
+            let rawWidth = cell.calculateButtonSize(basedOn: .height(buttonHeight), backgroundImage: backgroundImage, cellSpacing: 16)
+
+            // Ограничиваем максимальную ширину
+            let horizontalInset: CGFloat = 32 // например, 16 слева и 16 справа
+            let maxWidth = collectionView.frame.width - horizontalInset
+            let buttonWidth = min(rawWidth, maxWidth)
+
+            // Применяем размеры к кнопке
+            newButoon.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                newButoon.widthAnchor.constraint(equalToConstant: buttonWidth * 0.75),
+                newButoon.heightAnchor.constraint(equalToConstant: buttonHeight/3)
+            ])
+            print("Button size:", buttonWidth * 0.75, buttonHeight/3)
+
         }
         
         if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
